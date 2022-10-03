0d91a0f23026ffb65d71a4cdba52a9e53ef28626
Michael Spicer <>
2022-10-03 06:31:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+print("Hi, I am your AI Lilly, I want to give you some inspiration!")
+name = input("Let's start with your name, type your name here: ")
+starSign = input("Ok, what is your star sign? ")
+day = input("And what day is it where you are today? ")
+print("")
+if (starSign == "Aries" or starSign == "aries") and (day == "Monday" or day == "monday"):
+	if day == "Monday" or day == "monday": 
+		print("On no a Monday! Well just remember as a Aries,", name , "you are funny and you know it! Hang in there, only 4 more days until the weekend!")
+	else:
+		print("Ah... one of those days well it could be worse it could be a Monday! As a Aries", name, "I know your sense of humor will help you get through!")
+
+elif starSign == "Taurus" or starSign == "taurus": 
+	print("As a Taurus, ", name , "you are brave and you know it!")
+elif starSign == "Gemini" or starSign == "gemini": 
+	print("As a Gemini, ", name , "you are a boss and you know it!")
+elif starSign == "Cancer" or starSign == "cancer": 
+	print("As a Cancer, ", name , "you are determined and you know it!")
+elif starSign == "Leo" or starSign == "leo": 
+	print("As a Leo, ", name , "you are strong like a Lion!")
+elif starSign == "Virgo" or starSign == "virgo": 
+	print("As a Virgo, ", name , "you are clever and you know it!")
+elif starSign == "Libra" or starSign == "libra": 
+	print(day, "ha I see. As a Libra,", name , "don't forget how strong you are and you know it!")
+elif starSign == "Scorpio" or starSign == "scorpio": 
+	print(day, "ha I see. As a Scorpio,", name , "don't forget how resourceful you are, and you know it!")
+elif starSign == "Sagittarius" or starSign == "sagittarius":
+	print(day, "ha, I see. As a Sagittarius,", name , "don't forget how cool you are, and you know it!")
+elif starSign == "Capricorn" or starSign == "Capricorn": 
+	print(day, "ha I see. As an Aquarius,", name , "don't forget how good-looking you are!")
+elif starSign == "Aquarius" or starSign == "aquarius": 
+	print(day, "ha I see. As an Aquarius,", name , "don't forget how good-looking you are!")
+elif starSign == "Pisces" or starSign == "pisces": 
+	print(day, "ha I see. As a Pisces,", name , "you know how to start a party!")
+else:
+	print("Your entry did not compute, please try again", name)
